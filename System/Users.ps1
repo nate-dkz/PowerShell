dd015e1eb3c45d4a973a92d8b3a8b58ab237d9e4
Nathan <nathan@nathandarker.it>
2023-01-08 21:55:06 +0000

    Initial Commit

--- System/Users.ps1
+++
@@ -1,5 +0,0 @@
-# Returns the users logged into the machine
-query user
-
-# Uses the ID of the session and logs the user out of the machine
-logoff 8
---
+++ System/WMI Functions.ps1
@@ -0,0 +1,51 @@
+#Requires -Version 5.1
+function Get-Sysinfo {
+    [CmdletBinding()]
+    param()
+
+    Get-WmiObject -Class Win32_OperatingSystem |
+    Select-Object -Property Status,
+                            Manufacturer,
+                            @{n='OSBuild' ; e={$_.Caption}},
+                            OSArchitecture,
+                            Version,
+                            SerialNumber,
+                            @{n='HostName' ; e={$_.CSName}},
+                            BootDevice,
+                            SystemDevice,
+                            WindowsDirectory,
+                            SystemDirectory,
+                            RegisteredUser,
+                            NumberOfUsers,
+                            @{n='LocalDateTime' ; e={$_.ConverttoDateTime($_.lastbootuptime)}},
+                            @{n='InstallDate' ; e={$_.ConverttoDateTime($_.InstallDate)}},
+                            @{n='LastBootUpTime' ; e={$_.ConverttoDateTime($_.LastBootUpTime)}},
+                            NumberOfProcesses,
+                            @{n='TotalVisibleMemorySize(GB)' ; e={$_.TotalVisibleMemorySize / 1MB -as [int]}},
+                            @{n='FreePhysicalMemory(GB)' ; e={$_.FreePhysicalMemory / 1MB -as [int]}}
+}
+
+function Get-DiskInfo {
+    [CmdletBinding()]
+    param()
+    
+    Get-WmiObject -Class Win32_LogicalDisk |
+    Where-Object {$_.DriveType -eq 3} |
+    Select-Object -Property DeviceID,
+                            DriveType,
+                            @{n='Size(GB)' ; e={$_.size / 1GB -as [int]}},
+                            @{n='Free(GB)' ; e={$_.FreeSpace / 1GB -as [int]}}
+}
+
+function Get-ProcessorInfo {
+    [CmdletBinding()]
+    param()
+    
+    Get-WmiObject -Class Win32_Processor |
+    Select-Object -Property Manufacturer,
+                                Description,
+                                Name,
+                                NumberOfCores,
+                                NumberOfEnabledCore,
+                                NumberOfLogicalProcessors
+}
