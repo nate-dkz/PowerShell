53ddeb51a55b494ccfa3550e40a2c8e6d413ad66
Nathan <nathan@nathandarker.it>
2023-01-08 21:34:40 +0000

    Initial Commit

--- Security/New-PSPassword.ps1
+++ Security/New-PSPassword.ps1
@@ -1,14 +1,15 @@
 Function New-PSPassword {
-	
     [CmdletBinding()]
     Param (
         [int] $length = 15
     )
 
-    Begin {}
+    BEGIN {}
 
-    Process {
-        Write-Output ( -join ((0x30..0x39) + ( 0x41..0x5A) + ( 0x61..0x7A) | Get-Random -Count $length  | ForEach-Object { [char]$_ }) )
+    PROCESS {
+        Write-Output ( -join ((0x30..0x39) + ( 0x41..0x5A) + ( 0x61..0x7A) | 
+        Get-Random -Count $length  | 
+        ForEach-Object { [char]$_ }) )
     }
 
     END{}
