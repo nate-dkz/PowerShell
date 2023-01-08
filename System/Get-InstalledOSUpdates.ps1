02f644096f5368d327a92741866097d427546c09
Nathan <nathan@nathandarker.it>
2023-01-08 21:44:10 +0000

    Initial Commit

---
+++ System/Get-InstalledOSUpdates.ps1
@@ -0,0 +1,10 @@
+function Get-InstalledOSUpdates {
+    [CmdletBinding()]
+    param(
+        [string]$ComputerName = 'localhost'
+    )
+
+    Get-CimInstance -ClassName win32_quickfixengineering -ComputerName $ComputerName | 
+    Select-Object -Property @{n='HostName' ; e={$_.CSName}}, HotFixID, Description, Caption, InstalledOn, InstalledBy |
+    Sort-Object -Property InstalledOn -Descending
+}
