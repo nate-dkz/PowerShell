7bd4f6e2d5f19e8a8b9d6b9710669272078d598b
Nathan <nathan@nathandarker.it>
2023-01-08 20:21:56 +0000

    Initial Commit

---
+++ Active Directory/Check-ADUserInfo.ps1
@@ -0,0 +1,25 @@
+﻿function Get-ADUserInfo {
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory=$true)]
+        [string] $EmailAddress
+    )
+
+    # Define the regex pattern for email addresses
+    $Pattern = '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+
+    # Check if the email address matches the pattern
+    if ($EmailAddress -match $Pattern) {
+        Get-ADUser -Filter "EmailAddress -eq '$EmailAddress'" -Properties * |
+        Select-Object -Property DisplayName,
+                                DistinguishedName,
+                                EmailAddress,
+                                Enabled,
+                                LockedOut,
+                                LastLogonDate,
+                                PasswordLastSet
+    }
+    else {
+        Write-Output "Invalid email address, please check the email address and try again"
+    }
+}
